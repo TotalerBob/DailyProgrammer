f3c66e599869aa57af485026a4fad418bf100624
Maximilian König <masterking@windowslive.com>
2017-09-19 10:00:39 +0200

    Bugfix Number Input Firefox Challenge 1 Input changes Base for Challenge 3 created

--- js/problems/challenge_3.js
+++ js/problems/challenge_3.js
@@ -1,3 +1,12 @@
-function Init() {
-	$('#popup').css("display", "inline-block");
+var challenge = {
+	title: 'Path to Philosophy',
+	description: 'Clicking on the first link in the main text of a Wikipedia article not in parentheses, and then repeating the process for subsequent articles, usually eventually gets you to the Philosophy article. As of May 26, 2011, 94.52% of all articles in Wikipedia lead eventually to the article Philosophy. The rest lead to an article with no wikilinks or with links to pages that do not exist, or get stuck in loops.',
+	difficulty: 'hard',
+	reddit: 'https://www.reddit.com/r/dailyprogrammer/comments/6j7k3x/20170624_challenge_320_hard_path_to_philosophy/',
+	inputs: [{ type: 'text', name: 'c1_number', title: 'Wikipedia Article' }],
+	outputs: [],
+	init: function () { },
+	run: function () {
+
+	}
 }
